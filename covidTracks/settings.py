4146b62e3b1100085cfdf7e4adaf444140f62ba1
iamabhishektiwari <abhishek.tiwari.cer15@itbhu.ac.in>
2020-04-12 03:19:17 +0530

    host added -6

--- covidTracks/settings.py
+++ covidTracks/settings.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'covidTracks.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['TEMPLATES'],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
